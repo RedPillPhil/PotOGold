01d01ef97f1356b368a76cc9c3b1cd785f8d527e
RedPillPhil <47307224+RedPillPhil@users.noreply.github.com>
2022-06-20 14:56:48 -0400

    Update main.4b56eeea.chunk.js

--- static/js/main.4b56eeea.chunk.js
+++ static/js/main.4b56eeea.chunk.js
@@ -16,7 +16,7 @@
 				g = n(84),
 				m = n(86),
 				j = n(90),
-				s = n.p + "/static/media/FullLogo.e313bec7.png.png",
+				s = n.p + "",
 				p = n(1),
 				b = Object(d.a)("div")((function(t) {
 					var e = t.theme;
