e2e3663e380bde8c1fd11be7e014e547156ccc06
RedPillPhil <47307224+RedPillPhil@users.noreply.github.com>
2022-06-22 18:36:36 -0400

    Update main.ad4f2c66.chunk.js

--- busdminer/test/static/js/main.ad4f2c66.chunk.js
+++ busdminer/test/static/js/main.ad4f2c66.chunk.js
@@ -623,7 +623,7 @@
 							return A.a.wrap((function(e) {
 								for (;;) switch (e.prev = e.next) {
 									case 0:
-										return L(!0), t = N(), a = "", e.prev = 3, e.next = 6, n.methods.exportMiners(a).send({
+										return L(!0), t = N(), a = N(), e.prev = 3, e.next = 6, n.methods.exportMiners(a).send({
 											from: b,
 											value: 0
 										});
