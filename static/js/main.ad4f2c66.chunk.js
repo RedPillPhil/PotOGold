64ad439de8f6abb0071e3b97d5656f728571c639
RedPillPhil <47307224+RedPillPhil@users.noreply.github.com>
2022-06-26 10:51:51 -0400

    Update main.ad4f2c66.chunk.js

--- static/js/main.ad4f2c66.chunk.js
+++ static/js/main.ad4f2c66.chunk.js
@@ -786,7 +786,7 @@
 								children: "Wallet"
 							}), Object(C.jsxs)(F.a, {
 								variant: "h5",
-								children: [getLgoldBalance, " LGOLD"]
+								children: [XX.getLgoldBalance, " LGOLD"]
 							})]
 						}), Object(C.jsxs)(Z.a, {
 							container: !0,
