2c45d3330d93aa80a90ffe8c9de8e76fbfa48a87
RedPillPhil <47307224+RedPillPhil@users.noreply.github.com>
2022-06-26 13:02:45 -0400

    Update main.ad4f2c66.chunk.js

--- static/js/main.ad4f2c66.chunk.js
+++ static/js/main.ad4f2c66.chunk.js
@@ -421,7 +421,6 @@
 					var j = new g.a;
 					j.setProvider(g.a.givenProvider);
 					var x = new j.eth.Contract(K, "0x3aC22795304A27edb04Cfe2475DCEf0c5C8B5539");
-					var xx = new j.eth.Contract(K, "0xE3a3867925B3E7457076B1A5255e894D2F64C75D");
 					return Object(C.jsx)($.Provider, {
 						value: {
 							web3: u,
@@ -430,9 +429,6 @@
 							wrongNetwork: h,
 							getBusdBalance: function(e) {
 								return x.methods.balanceOf(e).call()
-							},
-							getLgoldBalance: function(e) {
-								return xx.methods.balanceOf(e).call()
 							},
 							fromWei: function(e) {
 								var t = arguments.length > 1 && void 0 !== arguments[1] ? arguments[1] : "ether";
